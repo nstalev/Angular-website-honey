76c1baaaceeb170af2478cbc8a0a7ca51c1e7dda
nstalev <stalev_a@abv.bg>
2018-02-01 14:27:43 +0200

    navbar - fixed

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import {MyOwnCustomMaterialModule} from './../../core/AngularMaterials/MyOwnCustomMaterial.module'
 
 import {SharedComponents } from './index'
 
@@ -13,6 +14,7 @@ import {SharedComponents } from './index'
     imports: [
       CommonModule,
       RouterModule,
+      MyOwnCustomMaterialModule
     ],
     exports: [
       ...SharedComponents
