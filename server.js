819a5c95d9fa21a7b776368f7a41034b75a9f465
nstalev <stalev_a@abv.bg>
2018-02-07 11:28:04 +0200

    update

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ const path = require('path')
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.Port || 8080);
+
+app.get('/*', function(req, res){
+        res.sendfile(path.join(__dirname + '/dist/index.html'));
+    })
